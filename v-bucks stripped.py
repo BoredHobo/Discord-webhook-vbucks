8366296c06307ea70f15c59c96e5b3d10198e56c
BoredHobo <108602226+BoredHobo@users.noreply.github.com>
2025-02-18 11:34:01 -0500

    Change to SS Beast filter. Add roles.

--- v-bucks stripped.py
+++ v-bucks stripped.py
@@ -7,19 +7,26 @@ import undetected_chromedriver as uc
 driver = uc.Chrome()
 driver.get("https://fortnitedb.com")
 
-#takes screenshot of the V-Bucks, and saves it as a png to wherever you decide, then closes Chrome
-driver.implicitly_wait(5)
-vbucks = driver.find_element(By.CLASS_NAME, "new_block_block.mt-0")
+#Clicks the pre-defined xxMistaBeastxx filter
+filter = driver.find_element(By.XPATH, "/html/body/main/div[1]/div/div[2]/div[2]/div/div[2]/div[1]/div[3]/div/div/span").click()
+
+# Finds table that displays the missions
+vbucks = driver.find_element(By.XPATH, "/html/body/main/div[1]/div/div[2]/div[2]/div/div[3]/table/tbody")
+
+# Scrolls about half-way down, may need to be tweaked based on your monitor
+driver.execute_script("window.scrollTo(0, 700)")
 vbucks.screenshot("/vbucks.png")
 driver.quit
 
 #uses Discord MarkDown to make a link and @ a couple people
 content = "[FortniteDB](<https://fortnitedb.com/>) <@> <@> <@> <@>"
 
-#without this, it can't @ anyone, could also change it to a role
+# without this, it can't @ anyone, could also change it to a role
 allowed_mentions = {
-    "users": ["", "", "", ""]
+    "users": ["user-id"],
+    "roles": ["role-id"]
 }
+
 #replace with your webhook url
 webhook = DiscordWebhook(url='Place URL Here', username="V-Bucks", content=content)
 
